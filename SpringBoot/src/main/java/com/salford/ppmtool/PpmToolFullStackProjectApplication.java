000854de0ce2ca2f9f1e0409b176db41223affd3
Roshan Pascal Raj <roshan.pascalraj@gmail.com>
2021-09-15 15:07:35 +0100

    First commit

---
+++ SpringBoot/src/main/java/com/salford/ppmtool/PpmToolFullStackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.salford.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolFullStackProjectApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/src/test/java/com/salford/ppmtool/PpmToolFullStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salford.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
